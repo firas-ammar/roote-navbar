11a2875d98a53d2c38515d2a9b8fd713572bdf7f
firas-ammar <firas.ammar.ing@gmail.com>
2019-09-03 14:02:43 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import { UncontrolledButtonDropdown, DropdownMenu, DropdownItem, DropdownToggle } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+      
+      render() {
+        return (
+          <Router>
+          <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/Services'} className="nav-link" >
+            <UncontrolledButtonDropdown>
+        <DropdownToggle >
+        Services
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem >For entrepreneurs</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>For Students</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>For Hobbyists</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledButtonDropdown>
+            </Link></li>
+            <li><Link to={'/Contact'} className="nav-link">Contact</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/Contact' component={Contact} />
+              <Route  exact path='/Services' component={Services} />
+          </Switch>
+          </div>
+          </Router>
+
+        );
+      }
+    }
 
 export default App;
---
+++ src/components/Services.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Services extends Component {
+  render() {
+    return (
+        <div> 
+                <h2 href='/'>Services</h2>
+
+      </div>
+    );
+  }
+}
+
+export default Services;
