3dd5c26861b6130ebe4ed8648b119a0b5748d895
firas-ammar <firas.ammar.ing@gmail.com>
2019-09-03 14:09:38 +0200

    Updates

---
+++ precache-manifest.d51fbb7400436613fd84309a2dce904b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1cce5c749e3c0cb608c7b1bac0c824",
+    "url": "/roote-navbar/index.html"
+  },
+  {
+    "revision": "3ad7d716f987d5a90027",
+    "url": "/roote-navbar/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e26cc597be35a81d0434",
+    "url": "/roote-navbar/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "3ad7d716f987d5a90027",
+    "url": "/roote-navbar/static/js/2.350d05de.chunk.js"
+  },
+  {
+    "revision": "e26cc597be35a81d0434",
+    "url": "/roote-navbar/static/js/main.eb0ea155.chunk.js"
+  },
+  {
+    "revision": "0175988dff301c475b0b",
+    "url": "/roote-navbar/static/js/runtime~main.13ec3c46.js"
+  }
+]);
